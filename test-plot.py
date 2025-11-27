0cc3c503a2f430b17359c315cd5b69d653ea9067
khian calderon <ckhianvictory@gmail.com>
2025-11-28 07:42:15 +0800

    Added sample plotting

--- test-plot.py
+++ test-plot.py
@@ -1,5 +1,13 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-mydata = pd.read_csv("sample-data.csv")
+df = pd.read_csv("sample-data.csv")
 
-print(mydata)
+income_by_year = df.groupby("year")["income"].mean()
+
+plt.plot(income_by_year.index, income_by_year.values, marker="o")
+plt.title("Average Household Income Over Time")
+plt.xlabel("Year")
+plt.ylabel("Income")
+plt.grid(True)
+plt.show()
